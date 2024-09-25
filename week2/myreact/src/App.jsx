e59ef61f5e6ec2609d253b914d335276095c55c0
jaeyoungmun <mjy0694@naver.com>
2024-09-25 20:14:48 +0900

    도감_react

--- week2/myreact/src/App.jsx
+++ week2/myreact/src/App.jsx
@@ -3,19 +3,27 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div style={{'display':'flex', 'flexDirection':'column'}}>
-      <section>
-        <div style={{display:'flex', flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
-            <img style={{innerWidth:'300px', height:'auto',borderRadius:'20%'}} src="./umc_image.png" alt="###"/>
-            <h2 style={{color:'green'}}>umc 카드</h2>
-            <p style={{color:'gray'}}>html css js</p>
-            <button style={{border:'none', backgroundColor:'palevioletred', borderRadius:'20%'}}>WEB</button>
+    <div className='section'>
+      <header className="header">
+        <a href="/">포켓몬 도감</a>
+      </header>
+      <main className="main">
+        <img src="../public/poketmon.png" alt="#" className="img" />
+        <div className="content">
+          <div className="content__title">망나뇽</div>
+          <div className="content__subtitle">늠름한 포켓몬</div>
+          <div className="content__name">망나용 | 포켓몬</div>
+          <div className="content__text">넓은 바다 어딘가를 거처로 삼아 날아 이동한다고 하지만
+          어디까지나 소문에 지나지 않는다.</div>
         </div>
-    </section>
+      </main>
+      <footer className="footer">Copyright@제리</footer>
+      
     </div>
   )
 }
