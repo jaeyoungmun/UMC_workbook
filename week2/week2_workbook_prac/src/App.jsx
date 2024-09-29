c255712b64c931e3b63042eed6eeb03d95ffdf4f
jae0_12 <113167437+jaeyoungmun@users.noreply.github.com>
2024-09-29 19:42:10 +0900

    react_prac_useState

--- week2/week2_workbook_prac/src/App.jsx
+++ week2/week2_workbook_prac/src/App.jsx
@@ -1,26 +1,37 @@
 
 import './App.css'
-import List from './components/List.jsx';
+import { useState } from 'react';
 
 function App() {
-  const nickname = '제리';
-  const meat = '고기';
-  const array = ['REACT', 'NEXT', 'VUE', 'SVELTE', 'ANGULAR', 'REACT-NATIVE'];
-  // const numbers = [1,2,3,4,5];
+  
+  const [person, setPerson] = useState({
+    name: '문재영',
+    age: 23,
+    nickname: 'jerry'
+  });
+
+  const updateCity = () => {
+    setPerson(prevPerson => ({
+      ...prevPerson,
+      city: 'Seoul'
+    }));
+  }
+  
+  const increaseAge = () => {
+    setPerson(prevPerson => ({
+      ...prevPerson,
+      age: prevPerson.age + 1
+    }));
+  }
   return (
     <>
-      <strong className="school">상명대학교</strong>
-      <p style={{color: "purple", fontWeight: "bold", fontSize: '3rem'}}>{nickname}/문재영</p>
-      <h1>{`${nickname}는 ${meat}를 좋아합니다</h1>`}</h1>
-      <ul>
-        {array.map((yaho, i) => {
-          return <List key={i} tech={yaho}/>
-          // return <li key={i}>{yaho}/</li> // ->List컴포넌트를 호출합니다
-        })}
-        {/* {numbers.map((num,i) => {
-          return <li key = {i}> {num}</li> ->List컴포넌트를 호출합니다
-        })} */}
-      </ul>
+      <h1>이름: {person.name}</h1>
+      <h2>나이: {person.age}</h2>
+      <h3>닉네임: {person.nickname}</h3>
+      {person.city && <h4>도시: {person.city}</h4> }
+      <button onClick={updateCity}>도시 추가</button>
+      <button onClick={increaseAge}>나이 증가</button>
+      
     </>
   )
 }
--- week2/week2_workbook_prac/src/components/List.jsx
+++ week2/week2_workbook_prac/src/components/List.jsx
@@ -1,6 +1,6 @@
 // props를 호출했을 때 
 const List = (props) => {
-    console.log(props)
+    console.log(props.tech)
     const { tech, name, food} = props;
    return (
      <li style={{listStyle: 'none'}}>
