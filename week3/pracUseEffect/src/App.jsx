33abaf751bbfa5a52eb9cfdbb2f58016ab5f6fb6
jae0_12 <113167437+jaeyoungmun@users.noreply.github.com>
2024-10-11 02:02:49 +0900

    FEAT: router, get요청 완료

--- week3/pracUseEffect/src/App.jsx
+++ week3/pracUseEffect/src/App.jsx
@@ -1,33 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import RootLayout from './root-layout/root-layout'
+import NotFound from './pages/notFound/NotFound'
+import HomePage from './pages/homePage/HomePage'
+import LogIn from './pages/login/LogIn'
+import SignUp from './pages/signup/SignUp'
+import Search from './pages/search/Search'
+import Movies from './pages/movies/Movies'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './App.css'
 
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout/>,
+    errorElement: <NotFound/>,
+    children: [
+      {
+        index: true,
+        element: <HomePage/>
+      },
+      {
+        path: 'login',
+        element: <LogIn/>
+      },
+      {
+        path: 'signup',
+        element: <SignUp/>
+      },
+      {
+        path: 'search',
+        element: <Search/>
+      },
+      {
+        path: 'movies',
+        element: <Movies/>
+      }
+    ]
+  }
+])
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router}/>
     </>
   )
 }
